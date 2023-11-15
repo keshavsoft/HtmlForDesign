12af9640eadb469439746694d6edb38e087acd10
Satish Addanki <satishaddanki321@gmail.com>
2023-11-15 23:52:20 +0530

    satish

--- AllHtml/WithTabs/Js/AddListeners/StartFunc.js
+++ AllHtml/WithTabs/Js/AddListeners/StartFunc.js
@@ -1,10 +1,17 @@
 import { StartFunc as StartFuncSendMessageButtonId } from "./SendMessageButtonId/1-ClickAssign.js";
 import { StartFunc as StartFuncInputUserNameId } from "./InputUserNameId/1-ClickAssign.js";
+import { StartFunc as StartFuncSendBroadcastOnlyMessageButtonId } from "./SendBroadcastOnlyMessageButtonId/1-ClickAssign.js";
+import { StartFunc as StartFuncSendBroadcastMessageButtonId } from "./SendBroadcastMessageButtonId/1-ClickAssign.js";
+import { StartFunc as StartFuncSendPrivateMessageButtonId } from "./SendPrivateMessageButtonId/1-ClickAssign.js";
 
 let StartFunc = () => {
     
     StartFuncSendMessageButtonId();
+    StartFuncSendBroadcastOnlyMessageButtonId();
+    StartFuncSendBroadcastMessageButtonId();
     StartFuncInputUserNameId();
+    StartFuncSendPrivateMessageButtonId();
+
 };
 
 export { StartFunc };
--- AllHtml/WithTabs/Js/ForWebSocket/ReceiveMessages/AsJson.js
+++ AllHtml/WithTabs/Js/ForWebSocket/ReceiveMessages/AsJson.js
@@ -14,6 +14,31 @@ let StartFunc = ({ inJsonData }) => {
         });
     }
 
+    if (inJsonData.MessageType === "OnlineClients") {
+        let jVarLocalJsonData = inJsonData.JsonData;
+        let jVarLocalInboxId = document.getElementById("OnlineInboxChatId");
+        jVarLocalInboxId.innerHTML = "";
+        jVarLocalJsonData.forEach(element => {
+            showOnlineClientsContent({ inMessage: element });
+        });
+    }
+
+    if (inJsonData.MessageType === "BroadcastOnly") {
+        let jVarLocalJsonData = inJsonData.JsonData;
+        showMessageContent({ inMessage: JSON.stringify(jVarLocalJsonData)});
+    }
+
+    if (inJsonData.MessageType === "BroadcastAll") {
+        let jVarLocalJsonData = inJsonData.JsonData;
+        showMessageContent({ inMessage: JSON.stringify(jVarLocalJsonData)});
+    }
+
+    if (inJsonData.MessageType === "PrivateMessage") {
+        let jVarLocalJsonData = inJsonData.JsonData;
+        console.log("data",jVarLocalJsonData);
+        showMessageContent({ inMessage: JSON.stringify(jVarLocalJsonData)});
+    }
+
 };
 
 function showContent({ inMessage }) {
@@ -26,6 +51,28 @@ function showContent({ inMessage }) {
     let jVarLocalInboxId = document.getElementById("inbox_chat");
     jVarLocalInboxId.appendChild(clon);
 
+};
+
+function showOnlineClientsContent({ inMessage }) {
+    let temp = document.getElementById("OnlineClientsId");
+    let clon = temp.content.cloneNode(true);
+    let jVarLocalP = clon.querySelector("p");
+    jVarLocalP.innerHTML = inMessage.id;
+    let jVarLocalClientNameId = clon.getElementById("ClientNameId");
+    jVarLocalClientNameId.innerHTML = inMessage.Name;
+    let jVarLocalInboxId = document.getElementById("OnlineInboxChatId");
+    jVarLocalInboxId.appendChild(clon);
+
+};
+
+function showMessageContent({ inMessage }) {
+    let temp = document.getElementById("IncomingMessageId");
+    let clon = temp.content.cloneNode(true);
+    let jVarLocalP = clon.querySelector("p");
+    jVarLocalP.innerHTML = inMessage;
+    let jVarLocalMessageHistoryId = document.getElementById("MessageHistoryId");
+
+    jVarLocalMessageHistoryId.appendChild(clon);
 };
 
 export { StartFunc };
